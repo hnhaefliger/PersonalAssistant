44ed1a35ab34144a5848c522b3ae70b7b829bb59
Henry Haefliger <54628404+hnhaefliger@users.noreply.github.com>
2020-09-04 08:48:18 +0200

    Add files via upload

--- TTS/Train.py
+++ TTS/Train.py
@@ -2,21 +2,22 @@ from TTS import Model
 from Data.TTS import Dataset
 
 class Trainer:
-    def __init__(self, dataset, batch_size=8, interval=[0,-1], loadfrom=False):
+    def __init__(self, dataset, batch_size=8, interval=[0,-1], loadfrom=False, teacher_forcing=lambda x: not(x % 5)):
         '''
         Prepare model for training
         '''
 
         self.model = Model.Model(loadfrom=loadfrom)
+        self.teacher_forcing = teacher_forcing
 
-        self.dataset = Dataset.Dataset(dataset, batch_size=batch_size, interval=interval)
+        self.dataset = Dataset.Dataset(dataset, self.model, batch_size=batch_size, interval=interval, teacher_forcing=self.teacher_forcing)
 
     def reloadDataset(self, dataset, batch_size=8, interval=[0,-1]):
         '''
         Change dataset (for training with segments of the dataset
         '''
         
-        self.dataset = Dataset.Dataset(dataset, batch_size=batch_size, interval=interval)
+        self.dataset = Dataset.Dataset(dataset, self.model, batch_size=batch_size, interval=interval, teacher_forcing=self.teacher_forcing)
 
     def train(self, epochs):
         '''
